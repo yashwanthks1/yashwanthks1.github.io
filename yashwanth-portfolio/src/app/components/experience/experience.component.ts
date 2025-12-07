6817d5a449bd73b3657e63343217c21c6e8f53b3
Yashwanth K S <78689701+yashwanthks1@users.noreply.github.com>
2025-12-07 17:38:53 +0530

    created portfolio and code is committed
    created portfolio and code is committed

---
+++ yashwanth-portfolio/src/app/components/experience/experience.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experience',
+  templateUrl: './experience.component.html',
+  styleUrls: ['./experience.component.css']
+})
+export class ExperienceComponent { }
